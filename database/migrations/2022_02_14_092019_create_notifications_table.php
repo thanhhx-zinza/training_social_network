251ddb5309293014533304cc7767d20d8b1a4bce
tungqn14 <vantung14.qn@gmail.com>
2022-03-07 09:43:23 +0700

    Fix migration

--- database/migrations/2022_02_14_092019_create_notifications_table.php
+++ database/migrations/2022_02_14_092019_create_notifications_table.php
@@ -22,7 +22,6 @@ class CreateNotificationsTable extends Migration
             $table->text('data');
             $table->timestamp('read_at')->nullable();
             $table->string('action')->nullable();
-            $table->string('type');
             $table->timestamps();
         });
     }
