3ffc3c0bf24d22809d9f4d367853d355c77c2292
laivanvo <laivanvo2000s@gmail.com>
2022-03-08 16:19:00 +0700

    Fix indent in view comment/edit

--- resources/views/app/comment/edit.blade.php
+++ resources/views/app/comment/edit.blade.php
@@ -1,14 +1,13 @@
-@php
+<?php
     $cmt = $post->id . 'post'
-@endphp
+?>
 <form class="formAjax" name="{{$cmt}}" action="{{route('posts.comments.update', [$post->id, $comment->id])}}" method="POST">
     @csrf
     @method('PUT')
     <div class="row">
-    <div class="input-group mb-3">
-        <input type="text" class="form-control" name="content" value="{{$comment->content}}">
-        <button class="btn btn-outline-secondary" type="submit">ok</button>
-    </div>
-
+        <div class="input-group mb-3">
+            <input type="text" class="form-control" name="content" value="{{$comment->content}}">
+            <button class="btn btn-outline-secondary" type="submit">ok</button>
+        </div>
     </div>
 </form>
